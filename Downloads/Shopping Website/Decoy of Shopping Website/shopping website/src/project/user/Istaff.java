d6ede5107563341facea5960db9c4681a3ad652f
Marin <marinsar88@gmail.com>
2026-04-07 14:41:33 +0700

    Fresh

--- Downloads/Shopping Website/Decoy of Shopping Website/shopping website/src/project/user/Istaff.java
+++
@@ -1,8 +0,0 @@
-package project.user;
-public interface Istaff{
-    String getStaffId();
-    String getStaffName();
-    String getStaffPassword();
-    String getRole();
-    boolean can(String action);
-}
